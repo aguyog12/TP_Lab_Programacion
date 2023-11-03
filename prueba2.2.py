97a7b4ac5e61bd7f9959a1896fc9a18fdc00bd3b
aguyog12 <143817819+aguyog12@users.noreply.github.com>
2023-11-03 17:03:55 -0300

    Add files via upload

---
+++ prueba2.2.py
@@ -0,0 +1,93 @@
+while True:
+    print("Ingrese la unidad de medida que desea utilizar\n1- En caso de metros\n2- En caso de centímetros\n3- En caso de pies")
+
+    unidad_a_utilizar = 0
+
+    while unidad_a_utilizar not in [1, 2, 3]:
+        try:
+            unidad_a_utilizar = int(input("Ingrese el número correspondiente: "))
+            if unidad_a_utilizar not in [1, 2, 3]:
+                print("Error: Ingrese un número válido (1, 2 o 3).")
+        except ValueError:
+            print("Error: Ingrese un número válido (1, 2 o 3).")
+
+    while True:
+        ancho = float(input("Ingrese el ancho: "))
+        if ancho > 0:
+            break
+        else:
+            print("El ancho debe ser un número positivo mayor que cero. Por favor, inténtelo de nuevo.")
+
+    while True:
+        largo = float(input("Ingrese el largo: "))
+        if largo > 0:
+            break
+        else:
+            print("El largo debe ser un número positivo mayor que cero. Por favor, inténtelo de nuevo.")
+
+    while True:
+        espesor = float(input("Ingrese el espesor: "))
+        if espesor > 0:
+            break
+        else:
+            print("El espesor debe ser un número positivo mayor que cero. Por favor, inténtelo de nuevo.")
+
+    volumen = 0
+
+    if unidad_a_utilizar == 1:
+        volumen = largo * ancho * espesor
+    elif unidad_a_utilizar == 2:
+        volumen = (largo / 100) * (ancho / 100) * (espesor / 100)
+    elif unidad_a_utilizar == 3:
+        volumen = (largo / 3.28084) * (ancho / 3.28084) * (espesor / 3.28084)
+
+    print(f"La superficie será de {volumen} m^3")
+
+    tipo_Deconcreto = ["1:2:2","1:2:3","1:2:4","1:3:4","1:3:6"]
+    while True:
+        tipo_concreto = input(f"{tipo_Deconcreto}\nCual el tipo de concreto que desea: ")
+
+        if tipo_concreto in tipo_Deconcreto:
+            break
+        else:
+            print("Opción no válida. Por favor, elija una opción de la lista.")
+
+    if tipo_concreto == "1:2:2":
+        print (f"Necesitará la siguiente cantidad de materiales")
+        cemento = (volumen * 8.4) * 1.05
+        arena = volumen * 0.67
+        grava = volumen * 0.67
+        agua = volumen * 220
+        print(f"se necesitaran:\n {round(cemento, 2)} bolsas de 50 kg,\n {round(arena, 2)} kg de arena,\n {round(grava, 2)} kg de grava,\n {round(agua, 1)} litros de agua.")
+    elif tipo_concreto == "1:2:3":
+        print (f"Necesitará la siguiente cantidad de materiales")
+        cemento = (volumen * 7) * 1.05
+        arena = volumen * 0.56
+        grava = volumen * 0.84
+        agua = volumen * 180
+        print(f"se necesitaran:\n {round(cemento, 2)} bolsas de 50 kg,\n {round(arena, 2)} kg de arena,\n {round(grava, 2)} kg de grava,\n {round(agua, 1)} litros de agua.")
+    elif tipo_concreto == "1:2:4":
+        print(f"Necesitará la siguiente cantidad de materiales")
+        cemento = (volumen * 6) * 1.05
+        arena = volumen * 0.48
+        grava = volumen * 0.96
+        agua = volumen * 170
+        print(f"se necesitaran:\n {round(cemento, 2)} bolsas de 50 kg,\n {round(arena, 2)} kg de arena,\n {round(grava, 2)} kg de grava,\n {round(agua, 1)} litros de agua.")
+    elif tipo_concreto == "1:3:4":
+        print(f"Necesitará la siguiente cantidad de materiales")
+        cemento = (volumen * 5.2) * 1.05
+        arena = volumen * 0.63
+        grava = volumen * 0.84
+        agua = volumen * 170
+        print(f"se necesitaran:\n {round(cemento, 2)} bolsas de 50 kg,\n {round(arena, 2)} kg de arena,\n {round(grava, 2)} kg de grava,\n {round(agua, 1)} litros de agua.")
+    elif tipo_concreto == "1:3:6":
+        print(f"Necesitará la siguiente cantidad de materiales")
+        cemento = (volumen * 4.2) * 1.05
+        arena = volumen * 0.5
+        grava = volumen * 1
+        agua = volumen * 160
+        print(f"se necesitaran:\n {round(cemento, 2)} bolsas de 50 kg,\n {round(arena, 2)} kg de arena,\n {round(grava, 2)} kg de grava,\n {round(agua, 1)} litros de agua.")
+
+    respuesta = input("¿Desea calcular otro volumen? (si/no): ")
+    if respuesta.lower() != "si":
+        break
